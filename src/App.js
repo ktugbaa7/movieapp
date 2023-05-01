65abc5711d24664ea087afde417b9a91ebc5eab8
Tuğba Koç <ktugbaa7@gmail.com>
2023-05-01 11:51:29 +0300

    update

--- src/App.js
+++ src/App.js
@@ -17,17 +17,33 @@ function App() {
   const fetchMovie = async (query) => {
     setMovieList([]);
 
-    const temp = await fetch(`http://www.omdbapi.com/?s=${query}&apikey=ff1dec5e`)
+    if (query == "" || undefined || null) {
+      alert("Hata.")
+      return
+    }
+
+    const filmList = await fetch(`http://www.omdbapi.com/?s=${query}&apikey=ff1dec5e`)
       .then(res => res.json())
       .then(data => {
-        return data.Search
+        if (data.Response == "True") {
+          return data.Search
+        }
+        return []
       })
 
-    setMovieList(temp);
+      if(filmList.length > 0) {
+        setMovieList(filmList);
+      } else {
+        alert("Sonuç bulunamadı.")
+      }
+      
   }
-
- // ilk yüklemede görünen
-
+  
+  // ilk yüklemede görünen
+useEffect(() => {
+    fetchMovie("dune")
+  }, [])
+  
   return (
     <Router>
       <div className="App">
@@ -42,7 +58,7 @@ function App() {
                 setSearch={setSearch}
               />
             </div>} />
-          <Route path="/movie/:id"  element={<MovieDetails />} />
+          <Route path="/movie/:id" element={<MovieDetails />} />
         </Routes>
       </div>
     </Router>
